ff29b034eb09c94ddc3cbe1a9f0e3de0fca5ad3b
adriancordero12 <adrianbolivarcordero@gmail.com>
2021-07-16 17:22:00 -0400

    Iniciando proyecto

---
+++ main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+function crearVentanaPrincipal(){
+    let ventanaPrincipal = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    ventanaPrincipal.loadFile('index.html');
+
+}
+
+app.whenReady().then(crearVentanaPrincipal);
